1b4d1ad5a9311583a3e05b5598d43b0fe6679391
Michael Lindner <michael.lindner@mthweb.de>
2021-03-18 11:58:51 +0100

    Daten laden

--- js/main.js
+++ js/main.js
@@ -23,17 +23,46 @@ $(document).ready(function(){
 
 function init()
 {
+    $("#Laden").click(function(){
+
+        getData();
+
+    });
+
+
+
      $("#cmdabsenden").click(function(){
       
         // setTimeout erzeugt verzögerung beim Start
     setTimeout(function(){    
+                // Zeigen des Modalen Fensters
             $("#demo-modal").modal();         
       },500);
         
-        
+        // Sichtbar und UNdichtbar im wechsel (Display:none)
         $(".team").toggle(1000);
            });
 
 }
 
+
+function getData()
+{
+//fetch('https://jsonplaceholder.typicode.com/todos/1')
+ // .then(response => response.json())
+ // .then(json => console.log(json))
+
+ // https://jsonplaceholder.typicode.com/users
+
+// Laden der Daten
+$.getJSON('https://jsonplaceholder.typicode.com/users',function(data){
+var items=[];
+console.log(data);
+
+})
+// Wenn Daten zurück kommen
+
+// Bei Fehler
+
+}
 
