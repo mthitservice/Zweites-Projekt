c7251468e4527a74e092d4872cd52be29aef7300
Michael Lindner <michael.lindner@mthweb.de>
2021-03-18 10:18:09 +0100

    JQuery

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Unser erster Javascript
+/*Mit vielen
+    Funktionen */
+
+    // Variablen zum Speichern von Werten
+    // Deklarieren einer Variablen
+    var x;
+    // Initialisieren einer Variable
+    x=1;
+
+    // Methoden
+     // Bedingungen 
+     // Schleifen
+     
+$(document).ready(function(){
+
+alert('test2');
+
+});
+
+
