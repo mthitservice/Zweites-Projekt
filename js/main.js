b68ba01b5c1a76082e3dbb87ad4f386808ad0b02
Michael Lindner <michael.lindner@mthweb.de>
2021-03-18 14:12:26 +0100

    Node

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ $.each(data,function(i,field){
 
 //$("#temp_mitarbeiter").tmpl(field).appendTo("#Kontakte");
 
-$("#Kontakte").append("<div class='card col-lg-3 col-md-3 col-sm-4 text-center'><img class='card-img-top rounded-circle' src='mensch3.png' alt='Mitarbeiterfoto'/><div class='card-body'><h4 class='card-title'>" + field.name +"</h4><p class='card-text'>Sportler</p></div></div>")
+$("#Kontakte").append("<div class='card col-lg-3 col-md-3 col-sm-4 text-center'><img class='card-img-top rounded-circle' src='mensch3.png' alt='Mitarbeiterfoto'/><div class='card-body'><h4 class='card-title'>" + field.name +"</h4><p class='card-text'>Sportler</p></div></div>");
 
 });
 
